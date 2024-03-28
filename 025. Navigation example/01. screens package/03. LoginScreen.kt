00f72bf9cc50ec581c87d39085a3ef5ab43e23ee
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2024-03-28 07:53:41 +0530

    Create 03. LoginScreen.kt

---
+++ 025. Navigation example/01. screens package/03. LoginScreen.kt
@@ -0,0 +1,57 @@
+package com.example.navigation24.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation24.navigation.Screens
+
+@Composable
+fun LoginScreen(navController: NavController, modifier: Modifier = Modifier) {
+    Column (
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Login Screen", fontSize = 40.sp)
+        Spacer(modifier.height(40.dp))
+        Button(onClick = {
+            navController.navigate(Screens.ScreenHomeRoute.route)
+        }) {
+            Text(text = "Login (go Home)", fontSize = 25.sp)
+        }
+        Spacer(modifier.height(40.dp))
+        Button(onClick = {
+            navController.navigate(Screens.ScreenForgetPassRoute.route)
+        }) {
+            Text(text = "Forgot password", fontSize = 25.sp)
+        }
+        Spacer(modifier.height(40.dp))
+        Button(onClick = {
+            navController.navigate(Screens.ScreenRegisterRoute.route)
+        }) {
+            Text(text = "Register", fontSize = 25.sp)
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LoginScreenPreview() {
+    val navController = rememberNavController()
+
+    LoginScreen(navController = navController, modifier = Modifier)
+}
