8ff95696123c525c9480505f38833b0ac2b15eea
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2024-03-29 06:54:30 +0530

    Create 03. MainActivity.kt

---
+++ 025. Navigation example/03. MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.navigation24
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation24.navigation.Nav
+import com.example.navigation24.ui.theme.Navigation24Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Nav()
+
+        }
+    }
+}
+
