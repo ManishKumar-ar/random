4c642aef973e82cba3185235ff4fe441887d8d22
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2024-04-04 07:12:20 +0530

    Add files via upload

---
+++ 030. Lottie Animation/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.navigation24
+
+import android.os.Bundle
+import android.widget.SearchView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.Lottie
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlin.random.Random
+
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.cartoon_characters))
+
+        var isPlaying by remember {
+            mutableStateOf(true)
+        }
+        val progress by animateLottieCompositionAsState(
+            composition = composition,
+            isPlaying = isPlaying
+        )
+        LaunchedEffect(key1 = progress) {
+            if (progress == 0f) {
+                isPlaying = true
+            }
+            if (progress == 1f) {
+                isPlaying = false
+            }
+        }
+
+        LottieAnimation(
+            composition = composition,
+            modifier = Modifier.size(500.dp)
+                .clickable {
+                    isPlaying = true
+                },
+          //  iterations = LottieConstants.IterateForever
+            progress = {
+                progress
+            }
+        )
+    }
+}
